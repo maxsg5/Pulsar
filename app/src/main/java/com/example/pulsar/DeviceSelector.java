886916094c6ecc42920a0447dacb137daa8a7244
Dakota Doolaege <dakotadoolaege@gmail.com>
2023-01-07 13:09:35 -0700

    New branch for selecting a BLE device

---
+++ app/src/main/java/com/example/pulsar/DeviceSelector.java
@@ -0,0 +1,14 @@
+package com.example.pulsar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeviceSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_selector);
+    }
+}
